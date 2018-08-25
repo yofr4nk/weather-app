a0fd97405ea1d97983cfec2cc25314df58495b89
Yofrank <yofrank.sanchez@agence.cl>
2018-08-25 12:52:26 -0300

    remove 10% to fail logic per request

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,47 +1,30 @@
 const fetch = require('node-fetch');
 const {getLocations} = require('./geolocationController');
-const RedisService = require('../redis-client/client');
 const BlueBird = require('bluebird');
-const errorMessage = 'How unfortunate! The API Request Failed';
 const {map} = require('lodash');
 const moment = require('moment-timezone');
 
 const getWeatherFromPositions = async () => {
-	try{
-		if (Math.random(0, 1) < 0.1) throw new Error(errorMessage);
-		const getPositions = await getLocations();
-		return BlueBird.map(Object.keys(getPositions), (country) => {
-			return getWeather({lat: getPositions[country].latitude, lon: getPositions[country].longitude})
-		}, {concurrency: 6})
-		.then(weathers => {
-			return map(weathers, city => {
-				return {
-					country: getPositions[`${city.latitude}/${city.longitude}`].address,
-					hour: passUNIXTimeToHour(city.currently.time, city.timezone),
-					temperature: city.currently.temperature
-				}
-			});
-		});
-	} catch(err) {
-		return saveErrorLogs(err)
-		.then(() => {
-			return getWeatherFromPositions()
-		}).catch(err => {
-			throw new Error(err);
-		});
-	}
+  const getPositions = await getLocations();
+  return BlueBird.map(Object.keys(getPositions), (country) => {
+    return getWeather({lat: getPositions[country].latitude, lon: getPositions[country].longitude})
+  }, {concurrency: 6})
+  .then(weathers => {
+    return map(weathers, city => {
+      return {
+        country: getPositions[`${city.latitude}/${city.longitude}`].address,
+        hour: passUNIXTimeToHour(city.currently.time, city.timezone),
+        temperature: city.currently.temperature
+      }
+    });
+  })
+  .catch(err => {
+    return BlueBird.reject(err);
+  });
 }
 
 const passUNIXTimeToHour = (unixTime, timezone) => {
 	return moment.unix(unixTime).tz(timezone).format('HH:mm');
-}
-
-const saveErrorLogs = async (err) => {
-	if(err.message === errorMessage) {
-    const redisClient = new RedisService();
-		return await redisClient.hsetAsync(new Date().getTime(), "api.errors", err.message);
-	}
-	throw new Error(err);
 }
 
 const getWeather = ({lat, lon}) => {
